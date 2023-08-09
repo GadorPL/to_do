02066822589cf7bac0a01ce50c14a5c12a6a4861
Michał Hewelt <mhewelt9@gmail.com>
2023-08-09 15:00:35 +0200

    add 'complete' functionality

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show' | 'display':
-            for item in todos:
-                item = item.title()
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index + 1}={item}"
+                print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter a todo: ")
             todos[number] = new_todo
-
+        case 'complete':
+            number = int(input("Number of the todo to mark complete: "))
+            todos.pop(number - 1)
         case 'exit':
             break
         case _:

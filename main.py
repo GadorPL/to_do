17a7815c3dcbb735987ac1bb3882c627b0789064
Michał Hewelt <mhewelt9@gmail.com>
2023-08-11 10:59:07 +0200

    store data in txt file

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-todos = []
+
 
 while True:
     user_action = input("Type add or show, edit or exit: ").strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 row = f"{index + 1}={item}"
                 print(row)
